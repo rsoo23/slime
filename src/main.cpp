54017184169b21ac4b7466b89cac5b245932c2ae
rsooAILENS <rongjie.soo@ailensgroup.com>
2026-01-26 23:32:43 +0800

    fix: remove particleGap and spawnProability from executable args

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,8 @@ int main(int ac, char** argv)
 {
     Config config;
 
-    if (ac >= 2) config.particleGap = std::atoi(argv[1]);
-    if (ac >= 3) config.spawnProbability = std::atof(argv[2]);
-    if (ac >= 4) config.pheromoneLifetime = std::atoi(argv[3]);
-    if (ac >= 5) config.particleSpeed = std::atoi(argv[4]);
+    if (ac >= 2) config.pheromoneLifetime = std::atoi(argv[1]);
+    if (ac >= 3) config.particleSpeed = std::atoi(argv[2]);
 
     Scene scene(config);
     scene.startScene();
