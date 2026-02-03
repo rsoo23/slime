efb9b0e1aa8d189f3e096d669ee9e1280ef9dcdb
rsooAILENS <rongjie.soo@ailensgroup.com>
2026-02-03 22:35:04 +0800

    refactor: remove pheromone related components

--- includes/components/Components.hpp
+++ includes/components/Components.hpp
@@ -3,23 +3,12 @@
 
 struct SlimeParticle {};
 
-struct Pheromone {};
-
 struct Position2D {
     Vector2 value;
 };
 
 struct Direction2D {
     Vector2 value;
-};
-
-struct Lifetime {
-    float remaining;
-    float total;
-};
-
-struct PheromoneRender {
-    Color color;
 };
 
 struct SlimeParticleRender {
